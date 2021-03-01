8fac84398c647024f0f53cb86dc9305e16c03c1d
sekercih <hkkskrc@gmail.com>
2021-03-02 00:13:28 +0300

    14.gun

---
+++ src/day14/C3_MethodCreation3.java
@@ -0,0 +1,17 @@
+package day14;
+
+public class C3_MethodCreation3 {
+
+	public static void main(String[] args) {
+		// Gorev: Verilen uc double sayinin ortalamasini alan bir method yazin. 
+				ortalama(61.5, 16.6, 41.41);
+
+			}
+			
+			public static void ortalama(double sayi1, double sayi2, double sayi3) {
+				
+				System.out.println("Girilen sayilarin ortalamasi: " + (sayi1+sayi2+sayi3)/2);
+
+	}
+
+}
