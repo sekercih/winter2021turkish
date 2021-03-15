afd08a6b524a0525f02b5d27aa93027dff75f9fd
sekercih <hkkskrc@gmail.com>
2021-03-15 23:42:27 +0300

    26.gun

---
+++ src/day26/C1_Arrays01.java
@@ -0,0 +1,10 @@
+package day26;
+
+public class C1_Arrays01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/day26/C3_ArrayList03.java
@@ -0,0 +1,10 @@
+package day26;
+
+public class C3_ArrayList03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
