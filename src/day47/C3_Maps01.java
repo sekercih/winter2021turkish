41207a8109fea22c6affac95fea9e1afc89c56c7
sekercih <hkkskrc@gmail.com>
2021-04-08 23:30:46 +0300

    day47

---
+++ src/day47/C3_Maps01.java
@@ -0,0 +1,17 @@
+package day47;
+
+import java.util.HashMap;
+
+public class C3_Maps01 {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> objeMap=new HashMap<>();
+		objeMap.put(101, "Ali,Peli,Java");
+		objeMap.put(102, "Halit,Yavuz,Java");
+		objeMap.put(103, "Ali,Yan,C#");
+		
+		System.out.println(objeMap);
+		
+	}
+
+}
